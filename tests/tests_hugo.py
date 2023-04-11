de2a53ec94686bd6d84b605d02ee4a6803417432
hugo <hugo.banderier@giub.unibe.ch>
2023-04-11 23:35:44 +0200

    f

--- tests/tests_hugo.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from sklearn.datasets import load_digits
-from simpsom import SOMNet
-from test_network import TestNetwork
-
-if __name__ == '__main__':
-    data = load_digits().data
-    net = SOMNet(4, 4, data, init='pca', PBC=True)
-    net.train('online')
